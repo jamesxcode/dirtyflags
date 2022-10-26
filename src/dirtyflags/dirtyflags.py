4977535627e1a8bf571a667dd56c40f5a32acc39
jamesxcode <james.x.johnson@gmail.com>
2022-10-25 21:34:36 -0400

    feature: complete refactor to support attributes of any datatype

--- src/dirtyflags/dirtyflags.py
+++ src/dirtyflags/dirtyflags.py
@@ -1,11 +1,17 @@
 #  SPDX-FileCopyrightText: Copyright (c) 2022. James J. Johnson <james.x.johnson@gmail.com>
 #  SPDX-License-Identifier: BSD-3-Clause
-# ------------------------------------------------------------------------------
 """
 dirtyflags is a simple Python decorator that tracks when and which object attributes have changed.
 
 """
+import logging
+import pickle
+from hashlib import blake2b, blake2s
+from platform import architecture
 from typing import Any
+
+# create a logger
+logger = logging.getLogger(__name__)
 
 
 def dirtyflag(cls) -> Any:
@@ -23,18 +29,22 @@ def dirtyflag(cls) -> Any:
     # declare a sentinel to hold empty objects, since None is a valid default
     _sentinel = object()
 
+    # choose the optimal blake2 for the system architecture (64-bit or 32-bit)
+    try:
+        blake2 = blake2b if architecture()[0] == '64bit' else blake2s
+    except (KeyError,) as error:
+        logger.error("Had trouble determing the architecture %error.", error)
+        blake2 = blake2s
+
     # grab the __setattr_ function off of the decorated class to use later
     old_setattr = getattr(cls, '__setattr__', None)
 
     # override the decorated class's __setattr__ method
     def __setattr__(self, name, value):
 
-        # grab the old value of the attribute being set, compare to the new, and
-        # mark the attribute as dirty if the values are different
-        old_val = getattr(self, name, _sentinel)
-        if old_val is not _sentinel and old_val != value:
-            # print("Old {0} = {1!r}, new {0} = {2!r}".format(name, old, value))
-            self.dirty_attrs.add(name)
+        # as attributes are populated, add them to the dirty attrs dict
+        # with original values
+        self.orig_attrs.setdefault(name, _dirty_hash(value))
 
         # call the wrapped class original setattr method to update the value
         if old_setattr:
@@ -43,9 +53,20 @@ def dirtyflag(cls) -> Any:
             # Old-style class
             raise AttributeError
 
-        # initialize the dirty flag tracking list
-        if getattr(self, 'dirty_attrs', None) is None:
-            self.dirty_attrs = set()
+    def _dirty_hash(any_parm: Any) -> str | None:
+        """
+        Utility function to calculate a custom hash that will work on even python datatypes
+        that are not hashable.
+
+        :param any_parm: The value of the attribue to calculate a hash for
+        :return: The custom hash value
+        """
+        try:
+            dhash = blake2(pickle.dumps(any_parm), digest_size=8).hexdigest()
+            return dhash
+        except (ValueError,) as err:
+            logger.error("Had an issue generating a hash code for the attribute error. %err", err)
+            return None
 
     def is_dirty(self) -> bool:
         """
@@ -55,9 +76,9 @@ def dirtyflag(cls) -> Any:
         :param self: the instance being checked
         :return: True if dirty, False otherwise
         """
-        return bool(self.dirty_attrs)
+        return bool(self.dirty_attrs())
 
-    def get_dirty_attrs(self) -> set | None:
+    def dirty_attrs(self) -> list | None:
         """
         Utility function to grab the list of instance attributes that
         have been modified and are dirty.
@@ -65,10 +86,12 @@ def dirtyflag(cls) -> Any:
         :param self: the instance object
         :return: a set of the dirty attributes, or None if there are no dirty attributes
         """
-        return getattr(self, 'dirty_attrs', None) if is_dirty(self) else None
+        return [key for key, val in self.__dict__.items() if _dirty_hash(val) != self.orig_attrs.get(key)]
 
     # modify the decorated class and return to caller
     setattr(cls, 'is_dirty', is_dirty)
-    setattr(cls, 'get_dirty_attrs', get_dirty_attrs)
+    setattr(cls, 'dirty_attrs', dirty_attrs)
+    setattr(cls, 'orig_attrs', {})
+
     cls.__setattr__ = __setattr__
     return cls
--- tests/test_dirtyflags.py
+++ tests/test_dirtyflags.py
@@ -7,13 +7,15 @@ def test_basic_object():
     # create object
     @dirtyflag
     class BasicObj():
-        def __init__(self, aint, bfloat, cstr):
+        def __init__(self, aint, bfloat, cstr, dlist, fdict):
             self.a = aint
             self.b = bfloat
             self.c = cstr
+            self.d = dlist
+            self.f = fdict
 
     # test things
-    bo = BasicObj(5, 3.14, 'Test String')
+    bo = BasicObj(5, 3.14, 'Test String', [1, 2], {'key1': 'val1'})
     assert not bo.is_dirty()
 
 
@@ -21,13 +23,15 @@ def test_basic_object_isdirty():
     # create object
     @dirtyflag
     class BasicObj():
-        def __init__(self, aint, bfloat, cstr):
+        def __init__(self, aint, bfloat, cstr, dlist, fdict):
             self.a = aint
             self.b = bfloat
             self.c = cstr
+            self.d = dlist
+            self.f = fdict
 
     # test things
-    bo = BasicObj(5, 3.14, 'Test String')
+    bo = BasicObj(5, 3.14, 'Test String', [1, 2], {'key1': 'val1'})
     bo.a = 99
     bo.b = 88.88
     assert bo.is_dirty()
@@ -37,17 +41,43 @@ def test_basic_object_dirtyattrs():
     # create object
     @dirtyflag
     class BasicObj():
-        def __init__(self, aint, bfloat, cstr):
+        def __init__(self, aint, bfloat, cstr, dlist, fdict):
             self.a = aint
             self.b = bfloat
             self.c = cstr
+            self.d = dlist
+            self.f = fdict
 
     # test things
-    bo = BasicObj(5, 3.14, 'Test String')
+    bo = BasicObj(5, 3.14, 'Test String', [1, 2], {'key1': 'val1'})
     bo.a = 12
     bo.c = "Changed String"
     assert (
-            'a' in bo.get_dirty_attrs()
-            and 'b' not in bo.get_dirty_attrs()
-            and 'c' in bo.get_dirty_attrs()
+            'a' in bo.dirty_attrs()
+            and 'b' not in bo.dirty_attrs()
+            and 'c' in bo.dirty_attrs()
+    )
+
+
+def test_list_and_dict_object_dirtyattrs():
+    # create object
+    @dirtyflag
+    class BasicObj():
+        def __init__(self, aint, bfloat, cstr, dlist, fdict):
+            self.a = aint
+            self.b = bfloat
+            self.c = cstr
+            self.d = dlist
+            self.f = fdict
+
+    # test things
+    bo = BasicObj(5, 3.14, 'Test String', [1, 2], {'key1': 'val1'})
+    bo.d.append(3)
+    bo.f['key1'] = "Changed Value 1"
+    assert (
+            'a' not in bo.dirty_attrs()
+            and 'b' not in bo.dirty_attrs()
+            and 'c' not in bo.dirty_attrs()
+            and 'd' in bo.dirty_attrs()
+            and 'f' in bo.dirty_attrs()
     )
